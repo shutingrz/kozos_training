82d2a50d5b0726e7c7e8ee976a40f2d7491c69a4
shutingrz <shutingrz@gmail.com>
2019-10-26 14:29:27 +0900

    03 静的変数テスト

--- osbook_03/03/bootload/main.c
+++ osbook_03/03/bootload/main.c
@@ -2,15 +2,19 @@
 #include "serial.h"
 #include "lib.h"
 
+volatile int value = 10;
+
 int main(void){
     serial_init(SERIAL_DEFAULT_DEVICE);
 
     puts("Hello, world!\n");
 
-    putxval(0x10, 0);
+    putxval(value, 0);
     puts("\n");
 
-    putxval(0xffff, 0);
+    value = 20;
+
+    putxval(value, 0);
     puts("\n");
     
     while(1){
