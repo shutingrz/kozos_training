399317a3fda7780e2d845ef303560fecdd266948
shutingrz <shutingrz@gmail.com>
2019-11-02 04:35:53 +0900

    不要なデバッグコードの削除

--- osbook_03/05/bootload/main.c
+++ osbook_03/05/bootload/main.c
@@ -19,18 +19,6 @@ static int init(void){
     return 0;
 }
 
-int global_data = 0x10;
-int global_bss;
-static int static_data = 0x20;
-static int static_bss;
-
-static void printval(void){
-    puts("global_data = "); putxval(global_data, 0); puts("\n");
-    puts("global_bss = "); putxval(global_bss, 0); puts("\n");
-    puts("static_data = "); putxval(static_data, 0); puts("\n");
-    puts("static_bss = "); putxval(static_bss, 0); puts("\n");
-}
-
 /**
  * メモリの16進ダンプ出力
 **/
