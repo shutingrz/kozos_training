94e56b3f6efb03f79402c08c9a4e397452400076
shutingrz <shutingrz@gmail.com>
2019-11-06 02:52:10 +0900

    09の準備

---
+++ osbook_03/09/os/main.c
@@ -0,0 +1,26 @@
+#include "defines.h"
+#include "kozos.h"
+#include "interrupt.h"
+#include "lib.h"
+
+/**
+ * システムタスクとユーザスレッドの起動
+**/
+static int start_threads(int argc, char *argv[]){
+    kz_run(test08_1_main, "command", 0x100, 0, NULL);
+    return 0;
+}
+
+int main(void){
+    INTR_DISABLE; //割り込み無効にする
+
+    puts("kozos boot succeed!\n");
+
+    //OSの動作開始
+    kz_start(start_threads, "start", 0x100, 0, NULL);
+
+    /* ここには戻ってこない */
+
+    return 0;
+}
+
