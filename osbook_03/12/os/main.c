788d7c1c5ac51711fb179e5e6676018896dcd86c
shutingrz <shutingrz@gmail.com>
2019-11-10 18:39:52 +0900

    12の準備

---
+++ osbook_03/12/os/main.c
@@ -0,0 +1,36 @@
+#include "defines.h"
+#include "kozos.h"
+#include "interrupt.h"
+#include "lib.h"
+
+
+/**
+ * システムタスクとユーザスレッドの起動
+**/
+static int start_threads(int argc, char *argv[]){
+
+    kz_run(test11_1_main, "test11_1", 1, 0x100, 0, NULL);
+    kz_run(test11_2_main, "test11_2", 2, 0x100, 0, NULL);
+
+    kz_chpri(15); //優先順位を下げて、アイドルスレッドに移行する
+    INTR_ENABLE; //割り込み有効にする
+    while (1){
+        asm volatile ("sleep"); //省電力モードに移行
+    }
+
+    return 0;
+}
+
+int main(void){
+    INTR_DISABLE; //割り込み無効にする
+
+    puts("kozos boot succeed!\n");
+
+    //OSの動作開始
+    kz_start(start_threads, "idle", 0, 0x100, 0, NULL);
+
+    /* ここには戻ってこない */
+
+    return 0;
+}
+
